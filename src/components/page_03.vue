b8fdf16dfd69dc60d45b0e69590de34467cfdce9
sihnrila <52959210+sihnrila@users.noreply.github.com>
2026-07-14 15:17:49 +0900

    fix: change Kakao Maps SDK URL from http to https

--- src/components/page_03.vue
+++ src/components/page_03.vue
@@ -70,7 +70,7 @@ export default {
       /* global kakao */
       script.onload = () => kakao.maps.load(this.initMap);
       script.src =
-        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=5715011e564f34008898ac0b2c497e94"; // 카카오 지도 API
+        "https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=5715011e564f34008898ac0b2c497e94"; // 카카오 지도 API
       document.head.appendChild(script);
     }
   },
